b2a5a3c673fa7dc74380f71c0c15678a19b5f3d9
TBoonX <tboonx@googlemail.com>
2013-01-02 02:31:46 +0100

    Update cluster.c
    hart die zweite

--- cluster.c
+++ cluster.c
@@ -93,7 +93,7 @@ int main(int argc, char** argv)
 
 	if( p_world < 2 || p_world%2 !=0 || n%p_world != 0)	// Gerade Anzahl Prozesse >=2 und n Vielfaches von p?
 	{
-		if( rank_world == 0)
+		if( rank_world == 10)
 		{
 			printf("Programm bitte mit geradzahliger Prozess-Anzahl >= 2 starten. Bzw. n ist kein Vielfaches von p.\n");
 		}
